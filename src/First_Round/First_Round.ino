09f5f6bfebf3ac08eb46cf858886716fefa4d1b3
tausif-sama <t.samin16@gmail.com>
2022-11-17 12:27:26 +0600

    wow

--- src/First_Round/First_Round.ino
+++ src/First_Round/First_Round.ino
@@ -1,20 +1,30 @@
 #include <NewPing.h>
 #include <PIDController.h>
 #include <ESP32Servo.h>
+#include <BluetoothSerial.h>
 #include <HUSKYLENS.h>
+#include <MPU6050_light.h>
+MPU6050 mpu(Wire);
+int yawAngle = 0, startAngle = 0;
+#define endAngle 1080
+unsigned long MPUtimer = 0, lapTimer = 0;
 
 Servo myservo;
 #define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
-NewPing sonarL(13, 13, MAX_DISTANCE), sonarR(14, 14, MAX_DISTANCE);     // 2 = right sensor, 3 = left
-
 PIDController pid; // Create an instance of the PID controller class, called "pid"
+BluetoothSerial mySerial;
 HUSKYLENS huskylens;
 
+
+NewPing sonarL(15, 15, MAX_DISTANCE), sonarR(16, 16, MAX_DISTANCE);     // 2 = right sensor, 3 = left
 #define IN1 4
 #define IN2 25
 #define PWM 2
 #define stdby 26
+#define servo 33
 #define button 14
+
+#define mainSpeed 120
 bool start = false;
 int dif, ID;
 int angle;
@@ -22,18 +32,20 @@ int cal, pwm;
 long duration, distance, RD, FD, LD;
 int turndis = 110;
 char dir;
-float p = 26, i = .1, d = 8;
+float p = 23, i = .1, d = 8;
 
 
 int sensorValue, output, lastOutput;
 unsigned long lastMillis = 0, zeroStartMillis = 0, highSpeedMillis = 0, lastTurnMillis = 0, stopMillis = 0;
 float alpha = 0.6;
-int speed = 90;
+int speed = 120;
 bool justTurned = false;
 byte turnCount = 0, orangeCount = 0, blueCount = 0;
+int timeToIgnore = 1000;
 
 void setup () {
   Serial.begin(115200);  // Some methods require the Serial.begin() method to be called first'
+  mySerial.begin("ESP32test");  // Some methods require the Serial.begin() method to be called first'
 
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -42,7 +54,13 @@ void setup () {
   pinMode(button, INPUT_PULLUP);
 
   Wire.begin();
-  while (!huskylens.begin(Wire)) {
+  while (mpu.begin() != 0) {
+    Serial.print(F("MPU6050 failed to initialize!"));
+  }
+  mpu.calcOffsets(); // gyro and accelero
+  lapTimer = millis();
+  while (!huskylens.begin(Wire))
+  {
     Serial.println(F("Begin failed!"));
     Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>I2C)"));
     Serial.println(F("2.Please recheck the connection."));
@@ -51,41 +69,42 @@ void setup () {
 
 
   pid.begin();          // initialize the PID instance
-  pid.setpoint(30);    // The "goal" the PID controller tries to "reach"
+  pid.setpoint(29);    // The "goal" the PID controller tries to "reach"
   pid.tune(p, i, d);  // Tune the PID, arguments: kP, kI, kD
-  myservo.attach(33);
-  myservo.write(95); // 57, 95, 140
+  myservo.attach(servo);
+  myservo.write(90); // 50, 90, 140
   dir = 'L';
   waitForN();
-  drive(68);
+  drive(70);
   byte leftCount = 0, rightCount = 0;
   while (1) {
+    MPU();
     delay(2);
     LD = sonarL.ping_cm();
-    if (LD == 0 || LD > 100) {
+    if (LD == 0 || LD > 200) {
       leftCount++;
-      LD = 200;
+      LD = MAX_DISTANCE;
     }
     RD = sonarR.ping_cm();
-    if (RD == 0 || RD > 100) {
+    if (RD == 0 || RD > 200) {
       rightCount++;
-      RD = 200;
+      RD = MAX_DISTANCE;
     }
-    if (leftCount > 6) {
+    if (leftCount > 10) {
       dir = 'L';
       break;
     }
-    if (rightCount > 6) {
+    if (rightCount > 10) {
       dir = 'R';
       break;
     }
     int error = LD - RD;
-    myservo.write(constrain(map(error, -80, 80, 95 - 39, 95 + 39), 95 - 39, 95 + 39));
-    //    Serial.print(LD);
-    //    Serial.print("    ");
-    //    Serial.print(RD);
-    //    Serial.print("    ");
-    //    Serial.println(error);
+    myservo.write(constrain(map(error, -80, 80, 50, 140), 50, 140));
+    //    mySerial.print(LD);
+    //    mySerial.print("    ");
+    //    mySerial.print(RD);
+    //    mySerial.print("    ");
+    //    mySerial.println(error);
     //    if (huskylens.request(3) || huskylens.request(4)) {
     //      if (huskylens.count(3) > 0 || huskylens.count(4) > 0) {
     //        turnCount++;
@@ -96,41 +115,43 @@ void setup () {
     //    }
   }
 }
-
+bool endAngleReached = false;
 void loop () {
-  if (huskylens.request(3) && millis() - lastTurnMillis > 1000) {
-    if (huskylens.count(3) > 0)
-      orangeCount++;
+  MPU();
+  if (yawAngle > endAngle - 30 && yawAngle < endAngle + 30) {
+    endAngleReached = true;
   }
-  if (huskylens.request(4) && millis() - lastTurnMillis > 1100) {
-    if (huskylens.count(4) > 0)
-      blueCount++;
-  }
-  if (orangeCount > 0 || blueCount > 0) {
-    orangeCount = 0;
-    blueCount = 0;
-    turnCount++;
-    lastTurnMillis = millis();
-    justTurned = true;
+  if (endAngleReached == true && (huskylens.request(3) || huskylens.request(4))) {
+    myservo.write(90);
+    drive(-150);
+    delay(100);
+    drive(0);
+    mySerial.println("3 laps completed!\n");
+    mySerial.print("Lap time: ");
+    int lapTime = (millis() - lapTimer) / 1000;
+    mySerial.print(lapTime);
+    mySerial.println(" seconds");
+    while (1)
+      getInputs();
   }
   if (justTurned == true && abs(output) < 15) {
-    Serial.println("\nSpeeding up!");
-    speed = 200;
+    mySerial.println("\nSpeeding up!");
+    speed = 255;
     highSpeedMillis = millis();
     justTurned = false;
   }
-  if (millis() - highSpeedMillis > 40 && speed > 120) {
-    speed = 90;
+  if (millis() - highSpeedMillis > 20 && speed > mainSpeed) {
+    speed = mainSpeed;
   }
-  if (turnCount >= 12) {
-    myservo.write(95);
-    drive(-200);
-    delay(100);
-    drive(0);
-    while (turnCount != 0) {
-      getInputs();
-    }
-  }
+  //  if (turnCount >= 12) {
+  //    myservo.write(95);
+  //    drive(-200);
+  //    delay(100);
+  //    drive(0);
+  //    while (turnCount != 0) {
+  //      getInputs();
+  //    }
+  //  }
   delay(1);
   //   pid.setpoint(19);
   //  Serial.print(dir);
@@ -139,43 +160,66 @@ void loop () {
   //  Serial.print("\t");
   //  Serial.print(output);
   //  Serial.print("\t");
-  Serial.print(orangeCount);
-  Serial.print("\t");
-  Serial.print(blueCount);
-  Serial.print("\t");
-  Serial.print(turnCount);
-  Serial.print("\t");
+  //  mySerial.print(orangeCount);
+  //  mySerial.print("\t");
+  //  mySerial.print(blueCount);
+  //  mySerial.print("\t");
+  //  mySerial.print(turnCount);
+  //  mySerial.print("\t");
 
 
   if (dir == 'R') {
-    pid.limit(-40, 42);   // Limit the PID output between 0 and 255, this is important to get rid of integral windup!
+    pid.limit(-40, 50);   // Limit the PID output between 0 and 255, this is important to get rid of integral windup!
     RD = sonarR.ping_cm();
-    if (RD == 0) RD = 200;
+    if (RD == 0) {
+      RD = MAX_DISTANCE;
+      drive(60);
+      delay(2);
+    }
 
     sensorValue = RD;  // Read the value from the sensor
     output = pid.compute(sensorValue);   // Let the PID compute the value, returns the optimal output
-    output = constrain(output, -40, 42);
+    output = constrain(output, -40, 50);
 
     myservo.write(95 + output);
-    drive(speed - (abs(output) / 1.9));
+    drive(speed - (abs(output) * 1.5));
   } else {
-    pid.limit(-42, 40);   // Limit the PID output between 0 and 255, this is important to get rid of integral windup!
+    pid.limit(-50, 40);   // Limit the PID output between 0 and 255, this is important to get rid of integral windup!
     LD = sonarL.ping_cm();
-    if (LD == 0)
-      LD = 200;
+    if (LD == 0) {
+      LD = MAX_DISTANCE;
+      drive(60);
+      delay(2);
+    }
     //    else
     //      sensorValue = alpha * LD + (1 - alpha) * sensorValue;
     sensorValue = LD;
     output = pid.compute(sensorValue);   // Let the PID compute the value, returns the optimal output
-    output = constrain(output, -42, 40);
+    output = constrain(output, -50, 40);
 
     myservo.write(95 - output);
-    drive(speed - (abs(output) / 1.9));
+    drive(speed - (abs(output) * 1.5));
   }
-  Serial.println(sensorValue);
+
+  if (sensorValue > 120 && millis() - lastTurnMillis > 1000) {
+    turnCount++;
+    lastTurnMillis = millis();
+    justTurned = true;
+  }
+
+  mySerial.println(yawAngle);
   //  const float alpha2 = 0.9;
   //  output = alpha2 * output + (1 - alpha2) * lastOutput;
   //  lastOutput = output;
+  /*if(LD>RD && LD>170)
+    {
+      myservo.write(104-60);
+      drive(190);
+      delay(700);
+      myservo.write(104+60);
+      drive(190);
+      delay(220);
+    } */
 
   //  Serial.print("RD = ");
   //  Serial.print(RD);
@@ -186,6 +230,19 @@ void loop () {
   //  Serial.print("PID OUTPUT=");
   //  Serial.println(output);
 
+  /* if(output<-25){
+     myservo.write(104-40);
+     drive(255);
+     //delay(1500);
+     myservo.write(104+40);
+     drive(200);
+     //delay(1000);
+     myservo.write(104);
+    }
+
+    else (FD<10){
+     drive(0);
+    }    */
 
   Serial.println(millis() - lastMillis);
   lastMillis = millis();
@@ -196,62 +253,99 @@ void loop () {
 
 
 void drive(int pwm) {
-  if (pwm >= 0) {
+  if (pwm < 0) {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
     analogWrite(PWM, constrain(abs(pwm), 0, 255));
     digitalWrite(stdby, HIGH);
-  } else {
+  } else if (pwm > 0) {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     analogWrite(PWM, constrain(abs(pwm), 0, 255));
+    digitalWrite(stdby, HIGH);
+  } else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    analogWrite(PWM, pwm);
     digitalWrite(stdby, HIGH);
   }
 }
 
 void getInputs() {
-  if (Serial.available() > 0) {
-    int received_char = Serial.read();
+  if (mySerial.available() > 0) {
+    int received_char = mySerial.read();
     //    Serial.println(char(received_char));
     switch (received_char) {
       case 112://p
-        p = Serial.parseFloat();
+        p = mySerial.parseFloat();
         pid.tune(p, i, d);  // Tune the PID, arguments: kP, kI, kD
         break;
       case 105://i
-        i = Serial.parseFloat();
+        i = mySerial.parseFloat();
         pid.tune(p, i, d);  // Tune the PID, arguments: kP, kI, kD
         break;
       case 100://d
-        d = Serial.parseFloat();
+        d = mySerial.parseFloat();
         pid.tune(p, i, d);  // Tune the PID, arguments: kP, kI, kD
         break;
       case 109://m
-        speed = Serial.parseInt();
+        speed = mySerial.parseInt();
         break;
       case 110://n
-        drive(150);
-        delay(50);
-        start = true;
+        if (start != true) {
+          //          startAngle = abs(mpu.getAngleZ());
+          lapTimer = millis();
+          drive(150);
+          delay(50);
+          start = true;
+        }
         break;
       case 111://o
         ESP.restart();
-        turnCount = 0;
-        drive(255);
-        delay(10);
+        break;
+
+      case 113://q
+        timeToIgnore = mySerial.parseInt();
+        mySerial.print("set to: ");
+        mySerial.println(timeToIgnore);
+        break;
+
+      case 114://r
+        mySerial.print("Current degree");
+        mySerial.println(yawAngle);
+        drive(-200);
+        delay(100);
+        drive(0);
+        waitForN();
         break;
     }
   }
 }
 
+void MPU() {
+  mpu.update();
+  if ((millis() - MPUtimer) > 50) {
+    if (start == false) {
+      startAngle = abs(mpu.getAngleZ());
+      mySerial.println(startAngle);
+    } else {
+      yawAngle = abs(mpu.getAngleZ()) - startAngle;
+    }
+    MPUtimer = millis();
+  }
+}
+
 void waitForN() {
   while (start == false) {
+    MPU();
+    //    startAngle = abs(mpu.getAngleZ());
     getInputs();
     if (digitalRead(button) == LOW) {
+      //          startAngle = abs(mpu.getAngleZ());
+      lapTimer = millis();
       drive(150);
       delay(50);
       start = true;
-      break;
     }
   }
 }
